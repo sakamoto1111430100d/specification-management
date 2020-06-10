afa0cfcfc436b5aeae8d21f890886aa7d7be3863
Sakamoto Takaaki <st.sari66@gmail.com>
2020-06-10 20:06:25 +0900

    Update company.rb

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,12 @@
 class Company < ApplicationRecord
-  has_many :items, through: :documents
+  validates :name, presence: true
   has_many :documents
+  has_many :company_users
+  has_many :users, through: :company_users
+
+  
+  def self.search(keyword)
+    return Company.all unless search
+    Company.where('name LIKE(?)', "%#{keyword}%")
+  end
 end
