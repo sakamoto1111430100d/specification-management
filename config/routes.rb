32a948bb4eb27bc70a952ab6cb8a628eda45bd12
Sakamoto Takaaki <st.sari66@gmail.com>
2020-06-10 20:07:01 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'searches#index'
-  resources :users, only: [:edit, :update]
-    resources :searches, only: [:index, :new, :create]
+  resources :users, only: [:edit, :update] do
+    resources :companies, only: :index
+    resources :items, only: :index
+    resources :searches, only: [:index, :new, :create] do
+      collection do
+        get 'search'
+      end
+    end
+  end
+end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
