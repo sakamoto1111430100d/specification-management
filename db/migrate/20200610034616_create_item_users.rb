284aa7b7006da1dddb0fbcff3ce91f3629eb2553
Sakamoto Takaaki <st.sari66@gmail.com>
2020-06-13 01:44:33 +0900

    Delete 20200610034616_create_item_users.rb

--- db/migrate/20200610034616_create_item_users.rb
+++
@@ -1,9 +0,0 @@
-class CreateItemUsers < ActiveRecord::Migration[5.0]
-  def change
-    create_table :item_users do |t|
-      t.references :user, index: true
-      t.references :company, index: true
-      t.timestamps
-    end
-  end
-end
