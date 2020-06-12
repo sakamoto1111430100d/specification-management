45ce8c894995647ebba5364e23547248c5375c93
Sakamoto Takaaki <st.sari66@gmail.com>
2020-06-12 14:34:59 +0900

    Update companies_controller.rb

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -2,13 +2,21 @@ class CompaniesController < ApplicationController
   def index
     @company = Company.find(params[:company_id])
     @documents = Document.where(company_id: params[:company_id])
-    @items = Item.find(@documents[0][:item_id])
   end
 
-  # def document(documents)
-  #   documents.each do |document|
-  #     return document.items
-  #   end
-  # end
+  def edit
+    @company = Company.find(params[:company_id])
+    @company_id = params[:company_id]
+  end
 
+  def update
+    company = Company.where(id: params[:id])
+    company.update(company_params)
+    redirect_to user_companies_path(company_id: params[:id], user_id: current_user.id, )
+  end
+
+  private
+  def company_params
+    params.require(:company).permit(:name, :office, :id)
+  end
 end
