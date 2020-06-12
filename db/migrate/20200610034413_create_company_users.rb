e0b6c020f010d423625f17d586fba5aa5966dc4e
Sakamoto Takaaki <st.sari66@gmail.com>
2020-06-13 01:44:31 +0900

    Delete 20200610034413_create_company_users.rb

--- db/migrate/20200610034413_create_company_users.rb
+++
@@ -1,9 +0,0 @@
-class CreateCompanyUsers < ActiveRecord::Migration[5.0]
-  def change
-    create_table :company_users do |t|
-      t.references :user, index: true
-      t.references :company, index: true
-      t.timestamps
-    end
-  end
-end
