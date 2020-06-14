ea7ad55255beb1f19cf49fc3a493a30c36d6944d
Sakamoto Takaaki <st.sari66@gmail.com>
2020-06-14 14:32:33 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   def index
     @item = Item.find(params[:items_id])
-    @documents = Document.where(item_id: params[:items_id])
+    @documents = Document.where(item_id: params[:items_id]).order("date ASC")
   end
 end
