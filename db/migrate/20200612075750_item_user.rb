699424878a879f2f5ae4f4bd6db3463240607055
Sakamoto Takaaki <st.sari66@gmail.com>
2020-06-15 14:32:19 +0900

    Database modification

--- db/migrate/20200612075750_item_user.rb
+++
@@ -1,6 +0,0 @@
-class ItemUser < ActiveRecord::Migration[5.0]
-  def change
-    drop_table :item_users
-    drop_table :company_users
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200612075750) do
+ActiveRecord::Schema.define(version: 20200612071004) do
 
   create_table "companies", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",       null: false
