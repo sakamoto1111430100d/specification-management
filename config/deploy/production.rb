ed41240c804275f6b6259e87d39fde7a747e7d30
Sakamoto Takaaki <st.sari66@gmail.com>
2020-06-15 17:59:35 +0900

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-
+server '54.199.113.217', user: 'ec2-user', roles: %w{app db web}
 
 # role-based syntax
 # ==================
@@ -59,5 +59,3 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-
-server '54.199.113.217', user: 'ec2-user', roles: %w{app db web}
