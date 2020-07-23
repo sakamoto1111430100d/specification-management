3dc52630b653b2aa4d1c3f8c529a722668eeaa1f
Sakamoto Takaaki <st.sari66@gmail.com>
2020-07-23 15:01:50 +0900

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,6 @@ module Specification
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
     # config.i18n.default_locale = :ja
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{ja.yml}').to_s]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
